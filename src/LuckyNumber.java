570b34a62cb3e6e09993bf668621d68094f9e508
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-05 01:39:04 -0700

    created a recursive method to check the number is lucky or not after the elimination of certain indexes starting from 2

--- src/LuckyNumber.java
+++ src/LuckyNumber.java
@@ -1,5 +1,19 @@
 public class LuckyNumber {
+    public static boolean recursiveCheck(int n, int count){
+        if(n<count){
+            return true;
+        }if(n%count ==0){
+            return false;
+        }
+        return recursiveCheck(n-n/2,count +1)
+    }
+
+    public boolean luckyNumber(int n){
+        return recursiveCheck(n,2);
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
