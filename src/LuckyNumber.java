db700d26eff4eeea9619bbf1f259d1563d42ca96
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-05 01:42:26 -0700

    annotated accordingly

--- src/LuckyNumber.java
+++ src/LuckyNumber.java
@@ -1,14 +1,23 @@
 public class LuckyNumber {
+
+
+    //get a boolean function to call it in the actual function
     public static boolean recursiveCheck(int n, int count){
+
+        // if my index is smaller than the count to be deleted,then its true as no deletion will take place
         if(n<count){
             return true;
+
+            //if the count is divisible by index then return false as deletion will take place
         }if(n%count ==0){
             return false;
         }
-        return recursiveCheck(n-n/2,count +1)
+        //recursively call the same process by updating the value of index and count.
+        return recursiveCheck(n-n/2,count +1);
     }
 
     public boolean luckyNumber(int n){
+        //then just start the count with two ACT
         return recursiveCheck(n,2);
 
     }
